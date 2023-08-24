db9f57ef504b9b3dbaaeffaecc731a44e032a54a
Vitalii Savenko <vitaly.savenko12@gmail.com>
2023-08-24 23:59:23 +0300

    HW_7_Add clean-folder ver 0.0.2

--- README.md
+++
@@ -1,1 +0,0 @@
-This package can clean your folder and move your files.
--- setup.py
+++ setup.py
@@ -5,10 +5,9 @@ setup(name='clean_folder',
       version='0.0.2',
       packeges=find_packages(),
       author='Savenko Vitalii',
-      author_email='vitaly.savenko12@gmail.com'
-      license=file.LICENCE
+      author_email='vitaly.savenko12@gmail.com',
+      license='MIT',
       description='clean folder from some folder',
-      long_description = file.README.md
       url='https://github.com/Spica12/HW_7_File_sorter',
       entry_points={'console_scripts': ['clean-folder = clean_folder.clean:main',
                                         'fill-files = clean_folder.file_generator:main_generator']},
